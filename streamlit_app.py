34bb8adce382c23599bc374989823faa2cdb99c0
shagun41328 <49994052+shagun41328@users.noreply.github.com>
2023-02-17 13:02:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,14 @@ streamlit.dataframe(fruits_to_show)
 
 
 
-import requests
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
+# its giving results ...... response <200>
+
+
+
+# new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
